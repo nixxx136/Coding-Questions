627bf17059a313e192034cfc2a958d51316e0b3e
XiangyuLi926 <lixy0926@gmail.com>
2016-10-05 11:59:25 -0700

    Update

--- PocketGemsQuestion1/PocketGemsQuestion1/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "PathFinder.h"
-
-int main(int argc, const char * argv[]) {
-    PathFinder::parseFile("/Users/Xiangyu/Desktop/Gitfold/Coding-Questions/PocketGemsQuestion1/PocketGemsQuestion1/input_2.txt");
-    
-    return 0;
-}
